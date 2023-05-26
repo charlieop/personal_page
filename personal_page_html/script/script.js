2e3d1bf5bdb696ca4d82a2e4c82ccb67b18211cd
charlieop <spchanae@connect.ust.hk>
2023-05-26 08:08:22 +0800

    fixed previous commits

---
+++ personal_page_html/script/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World!");
+  init();
+});
+
+function init() {
+  //   var navToggle = document.querySelector("#nav-toggle");
+  // navToggle.addEventListener("change", () => {
+  //   if (navToggle.checked) {
+  //     document.querySelector("body").style.overflow = "hidden";
+  //     console.log("scroll disabled");
+  //   } else {
+  //     document.querySelector("body").style.overflowY = "auto";
+  //   }
+  // });
+}
