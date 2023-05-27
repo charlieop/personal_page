803a0c2e4aaf5eee7398ebc43e8a133380a85cec
charlieop <spchanae@connect.ust.hk>
2023-05-27 15:11:07 +0800

    nav bar finished

--- personal_page_html/script/script.js
+++ personal_page_html/script/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function init() {
   // scrollLock();
   stickNav();
+  changeCurrent();
 }
 
 // disable scrolling when nav opens
@@ -23,33 +24,57 @@ function scrollLock() {
 
 // change the nav after scrolling
 function stickNav() {
-  const mainContent = document.querySelector(".home");
   const header = document.querySelector("header");
+  const navToggle = document.querySelector("#nav-toggle");
+  var lastScroll = 0;
 
-  awayObserver = new IntersectionObserver(
-    (e) => {
-      if (e[0].isIntersecting) {
-        header.classList.add("away");
-      } else {
-        header.classList.remove("away");
-      }
-      console.log("Visiable?" + !e[0].isIntersecting);
-      // header.classList.toggle("away", !e[0].isIntersecting);
-    },
-    { rootMargin: "0% 0px -100% 0px" }
-  );
+  window.addEventListener("scroll", () => {
+    var currentScroll = window.scrollY;
+    if (currentScroll <= 10) {
+      header.classList.remove("scroll-up");
+      header.classList.remove("scroll-down");
+    } else if (
+      currentScroll > lastScroll &&
+      !header.classList.contains("scroll-down") &&
+      !navToggle.checked
+    ) {
+      header.classList.remove("scroll-up");
+      header.classList.add("scroll-down");
+    } else if (
+      currentScroll < lastScroll &&
+      !header.classList.contains("scroll-up")
+    ) {
+      header.classList.remove("scroll-down");
+      header.classList.add("scroll-up");
+    }
 
-  const contentObserver = new IntersectionObserver(
-    (e) => {
-      console.log(e[0].isIntersecting);
-      header.classList.toggle("sticking", !e[0].isIntersecting);
-      header.classList.remove("away");
-    },
-    { rootMargin: "-20% 0px 0px 0px" }
-  );
-
-  awayObserver.observe(mainContent);
-  contentObserver.observe(mainContent);
+    lastScroll = currentScroll;
+  });
 }
 
-function changeCurrent() {}
+function changeCurrent() {
+  const sections = document.querySelectorAll("section");
+  const navItems = document.querySelectorAll(".nav-item");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        console.log(entry.target.id + ": " + entry.isIntersecting);
+        if (entry.isIntersecting) {
+          const navItem = document.querySelector(
+            "#nav-" + entry.target.id
+          );
+          navItems.forEach((otherNavItem) => {
+            otherNavItem.classList.remove("current");
+          });
+          navItem.classList.add("current");
+        }
+      });
+    },
+    { rootMargin: "-35% 0px -65% 0px" }
+  );
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+}
